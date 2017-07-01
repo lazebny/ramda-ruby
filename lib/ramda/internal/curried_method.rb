14da5a48c7e5c18aaa06e729349f791ec11959a5
Vadim Lazebny <vadim.lazebny@gmail.com>
2017-07-01 18:10:16 +0300

    CleanUp: removed redundant code from curried_method

--- lib/ramda/internal/curried_method.rb
+++ lib/ramda/internal/curried_method.rb
@@ -36,31 +36,8 @@ module Ramda
         end
       end
 
-      def curried_method_v2(name, &block)
-        fn = Ramda::Internal::FunctionWithArity.call(block.arity) do |*args|
-          if args.empty?
-            block
-          else
-            yield(*args)
-          end
-        end
-
-        define_singleton_method(name, &fn.curry)
-      end
-
       def debug_log(name, args, result)
         puts "-> #{name}(#{args.join(', ')}) # #{result}"
-      end
-
-      def curried_method_v1(name, &block)
-        define_singleton_method(name) do |*args|
-          curried = block.curry
-          # curried.define_singleton_method(:origin_arity) { block.arity }
-
-          return curried if args.empty?
-
-          curried.call(*args)
-        end
       end
     end
   end
